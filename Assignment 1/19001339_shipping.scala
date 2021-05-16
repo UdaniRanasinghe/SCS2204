3b040132412970bee329915a4217c7a04e9d8fa7
UdaniRanasinghe <62123762+UdaniRanasinghe@users.noreply.github.com>
2021-05-16 17:08:15 +0530

    Update and rename shipping.scala to 19001339_shipping.scala

--- Assignment 1/19001339_shipping.scala
+++ Assignment 1/19001339_shipping.scala
@@ -1,11 +1,16 @@
+//Q3
+
 object shipping extends App {
   def cost (x : Int) : Double = {
+    
     var sum:Double = x*24.95
     sum = sum*60/100 //discounted price
     sum+= 3  //shipping cost for first 50 copies
 
     var additional:Int = x-50
-    sum += additional * 0.75
+    if(additional>0){
+      sum += additional * 0.75
+    }
     return sum
 
   }
